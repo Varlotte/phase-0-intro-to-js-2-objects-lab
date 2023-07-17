e0713fa56f4003b0f1141148b863422313a14da4
Charlotte Bush <clangbush@gmail.com>
2023-07-17 17:00:02 -0400

    passes 7 tests

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution in this file!
+const employee = {
+    name : "Sam",
+    streetAddress : "11 Broadway"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+        //can I add an object in here that says name: Sam and streetaddress: 11 Broadway
+      };
+
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+};
+
+
+function deleteFromEmployeeByKey(employee, key){
+const emp2 = {
+        ...employee
+    };
+
+    delete emp2[key]
+    return emp2;
+};
+//test says let newEmployee = deleteFromEmployeeByKey(employee, 'name');
+//I want to employees to return name: Sam and streetaddress: 12 Broadway
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+
+    return employee;
+}
+//let newEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'name');
+//expect(employee).to.equal(newEmployee);
